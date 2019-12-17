443e9c0add799f1b5421bc75852c22cd28fe351f
christine <christine.winge@gmail.com>
2019-12-17 18:27:29 +0100

    .

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-$("#check-form").click((e)=> {
+$("#check-form").click((e) => {
     e.preventDefault();
     var passord = "horntail";
     if ($("#passord").val() == passord) {
-        window.location.href="nox.html";
+        window.location.href = "nox.html";
+    } else {
+        window.alert("Feil passord!");
     }
 });
 
-$("#decrypt-form").click((e)=> {
+$("#decrypt-form").click((e) => {
     e.preventDefault();
     var passord = "alohomora";
     if ($("#decrypt").val() == passord) {
-        window.location.href="trelawney.html";
+        window.location.href = "trelawney.html";
+    } else {
+        window.alert("Feil passord!");
     }
 });
 
-$("#trelawney-form").submit((e)=> {
+$("#trelawney-form").submit((e) => {
     e.preventDefault();
     var passord = "butterbeer";
     if ($("#trelawneyPassord").val() == passord) {
-        window.location.href="merpeople.html";
+        window.location.href = "merpeople.html";
+    } else {
+        window.alert("Feil passord!");
     }
 });
 
-$("#selkie-form").submit((e)=> {
+$("#selkie-form").submit((e) => {
     e.preventDefault();
     var passord = "mariahcarey";
     if ($("#selkie").val() == passord) {
-        window.location.href="cup.html";
+        window.location.href = "stenography.html";
+    } else {
+        window.alert("Feil passord!");
+    }
+});
+
+$("#sten-form").submit((e) => {
+    e.preventDefault();
+    var passord = "bowtruckle";
+    if ($("#stenographyPassord").val() == passord) {
+        window.location.href = "cup.html";
+    } else {
+        window.alert("Feil passord!");
     }
 });
